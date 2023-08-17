3849bbdad037661ede61eade83da4b723ef93663
Jhonathan <guarino90@gmail.com>
2023-08-17 18:21:55 -0300

    modified paths on javascript files

--- src/js/services/repositories.js
+++ src/js/services/repositories.js
@@ -1,4 +1,4 @@
-import { baseUrl, repositoryQuantity } from "/src/js/variables.js";
+import { baseUrl, repositoryQuantity } from "../variables.js";
 
 async function getRepositories(userName) {
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoryQuantity}`);
